8dbb7d17cc109bcc7c4bd9701ad2b3880b5c907a
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-19 13:01:08 +0200

    add flags

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -25,16 +25,7 @@ export default {
         <ul>
             <li>{{ movie.title }}</li>
             <li> {{ movie.original_title }}</li>
-            <li :v-if="movie.original_language == 'it'">
-                    <img src="../assets/img/flag_italy.png" alt="">
-            </li>
-            <li :v-else-if="movie.original_language == 'en'">
-                <img src="../assets/img/flag_us.png" alt="">
-                <img src="../assets/img/flag_uk.png" alt="">
-            </li>
-            <li :v-else-if="movie.original_language == 'fr'">
-                <img src="../assets/img/flag_france.png" alt="">
-            </li>
+            <MovieLanguage :movie="movie"/>
             <li>{{ movie.vote_average }}</li>
 
         </ul>
