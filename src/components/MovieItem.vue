81d97511c1b573633f5c76926c276d98a1cd8c2c
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-19 12:53:26 +0200

    try to add flag on movie_language

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,6 +1,11 @@
 
 <script>
+import { store } from "../store";
+import MovieLanguage from "./MovieLanguage.vue"
 export default {
+    components: {
+        MovieLanguage
+    },
     name: "MovieItem",
     props: {
         movie: Object
@@ -8,7 +13,9 @@ export default {
     data() {
 
 
-        return {}
+        return {
+            store
+        }
     }
 }
 </script>
@@ -18,7 +25,16 @@ export default {
         <ul>
             <li>{{ movie.title }}</li>
             <li> {{ movie.original_title }}</li>
-            <li> {{ movie.original_language }}</li>
+            <li :v-if="movie.original_language == 'it'">
+                    <img src="../assets/img/flag_italy.png" alt="">
+            </li>
+            <li :v-else-if="movie.original_language == 'en'">
+                <img src="../assets/img/flag_us.png" alt="">
+                <img src="../assets/img/flag_uk.png" alt="">
+            </li>
+            <li :v-else-if="movie.original_language == 'fr'">
+                <img src="../assets/img/flag_france.png" alt="">
+            </li>
             <li>{{ movie.vote_average }}</li>
 
         </ul>
---
+++ src/components/MovieLanguage.vue
@@ -0,0 +1,29 @@
+
+<script>
+import { store } from "../store";
+export default {
+    name: "MovieLanguage",
+    data() {
+
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <li :v-if="movie.original_language == 'it'">
+        <img src="../assets/img/flag_italy.png" alt="">
+    </li>
+    <li :v-else-if="movie.original_language == 'en'">
+        <img src="../assets/img/flag_us.png" alt="">
+        <img src="../assets/img/flag_uk.png" alt="">
+    </li>
+    <li :v-else-if="movie.original_language == 'fr'">
+        <img src="../assets/img/flag_france.png" alt="">
+    </li>
+</template>
+
+
+<style lang="scss" scoped></style>
