e468cdb728b832e6eeb9573e293b9043383a5047
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-19 11:44:09 +0200

    add input form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 
 <script>
+import { store } from './store.js';
 export default {
   data () {
     
+    return {
+      store
+    }
+  }, 
+  mounted() {
+        this.store.callAPI(this.store.API_URL + this.store.searchValue)
 
-    return {}
-  }
+    }
 }
 </script>
 
 <template>
   <div>
-
+    <input type="text" v-model="this.store.searchValue">
+    <button>Click</button>
   </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -3,20 +3,21 @@ import axios from "axios"
 
 export const store = reactive({
     loading: true,
-    searchValue: "",
+    searchValue: "c",
     movies: null,
-    API_URL: "https://api.themoviedb.org/3/search/movie?api_key=85f7fb5ef1ff1562662336a11a351b9b",
+    API_URL: "https://api.themoviedb.org/3/search/movie?api_key=85f7fb5ef1ff1562662336a11a351b9b&query=",
     callAPI(url) {
-      axios 
-        .get(url)
-        .then(response => {
-            this.loading = false
-         
-        })
-        .catch(error => {
-                    console.log(error);
-                    console.error(error.message);
-                })
-                
-    }, 
-  })
+        axios
+            .get(url)
+            .then(response => {
+                this.loading = false,
+                    this.movies = response.data.results
+            })
+            .catch(error => {
+                console.log(error);
+                console.error(error.message);
+            })
+
+    },
+   
+})
