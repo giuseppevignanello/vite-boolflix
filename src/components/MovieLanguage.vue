eb0192b94c4ee9ba962e323bbe25dd1f7625aa78
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-30 14:54:08 +0200

    fix flag img path

--- src/components/MovieLanguage.vue
+++ src/components/MovieLanguage.vue
@@ -9,7 +9,7 @@ export default {
     data() {
         return {
             store,
-            flagPath: "../../img/",
+            flagPath: "../../public/img/",
             languages: [
                 "it",
                 "en",
